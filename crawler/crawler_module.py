aa3bbe1edb206a319177d6f6b6cfef4bb9657f97
Jackson_Kang <goodday1478@naver.com>
2018-05-24 16:50:16 +0900

    hangul, alphabet, numbers, symbols are allowed

--- crawler/crawler_module.py
+++ crawler/crawler_module.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 import urllib.request
 import urllib.parse
 import random
+import re
+from hanja import hangul
 
 
 class Crawler_Module:
@@ -45,20 +47,15 @@ class Crawler_Module:
             items = parsed_XML.find_all('item')
 
             result_list = []
-
             count = 1
+
+            parse_chars = "[^가-힣0-9a-zA-Z\\s~`!@#$%^&*()-=_+ \\ |[\]{};':\",./<>?]"
 
             for item in items:
                 title = self._strip_tag(item.title.get_text(strip=True))
                 description = self._strip_tag(item.description.get_text(strip=True))
-                temp_description = ""
-                temp_list = []
 
-                for char in description:
-                    temp_list.append(char)
-
-                for char in temp_list:
-                    temp_description = temp_description + char
+                temp_description = re.sub(parse_chars, '', description)
 
                 if count%10 == 0:
                     temp_string = title + "\n" + temp_description
@@ -66,26 +63,33 @@ class Crawler_Module:
                     temp_string = title + "\n" + temp_description + "\n\n"
 
                 result_list.append(temp_string)
+
             return result_list
 
         else:
             print("Crawler Error: " + str(status_code))
             return -1
 
+    # issue -> convert chinese and korean into korean
+
     # return a parsed crawling results
 
-
     def _strip_tag(self, target_string):
+
         target_string = self._tag_converter(target_string, "&quot;", "\"")
         target_string = self._tag_converter(target_string, "<b>", "")
         target_string = self._tag_converter(target_string, "</b>", "")
         target_string = self._tag_converter(target_string, u'\xa0', ' ' )
         target_string = self._tag_converter(target_string, "&amp;", "&")
+        target_string = self._tag_converter(target_string, "&lt;", "<")
+        target_string = self._tag_converter(target_string, "&gt;", ">")
+        target_string = self._tag_converter(target_string, "&nbsp;", " ")
 
         return target_string
 
+
     def _tag_converter(self, target_string, target_tag, converting_tag):
-        # convert &quot; into " and erace <br> or </br>.
+        # convert &quot; into " and erase <br> or </br>.
         temp_list = target_string.split(target_tag)
         result = converting_tag.join(temp_list)
 
@@ -114,5 +118,15 @@ class Crawler_Module:
             if tuple[1] == 1:
                 min_list.append(tuple[0])
 
-        return random.choice(min_list)
+        while True:
+            return_val = random.choice(min_list)
+            flag = 0
+            for ch in return_val:
+                if hangul.is_hangul(ch):
+                    flag += 1
+            if flag == len(return_val):
+                break
+
+
+        return return_val
 
