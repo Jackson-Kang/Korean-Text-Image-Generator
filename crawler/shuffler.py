10853b4b0e8fe23fe37ab0a2fdbfd5be52d52c1c
Jackson_Kang <goodday1478@naver.com>
2018-06-21 23:45:57 +0900

    path edited

--- crawler/shuffler.py
+++ crawler/shuffler.py
@@ -8,13 +8,15 @@ if __name__ == "__main__":
 
     data = {}
 
-    with open('./data/generated_img_data/desc.json', encoding="utf-8") as json_file:
+    desc_json_path = '/home/goodday1478/data/img_data/generated_img_data/data/desc.json'
+
+    with open(desc_json_path, encoding="utf-8") as json_file:
         data = json.load(json_file)
 
     random.shuffle(data["train"])
     random.shuffle(data["test"])
 
-    with open('./data/generated_img_data/desc.json', 'w') as outfile:
+    with open(desc_json_path, 'w') as outfile:
         json.dump(data, outfile)
 
     print(data)
