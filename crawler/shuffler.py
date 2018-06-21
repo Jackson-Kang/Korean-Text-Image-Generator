23338947e0ecf2f7d36d6a686a4177326f889a15
Jackson_Kang <goodday1478@naver.com>
2018-06-21 23:52:32 +0900

    shuffler bug edited

--- crawler/shuffler.py
+++ crawler/shuffler.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
 
     desc_json_path = '/home/goodday1478/data/img_data/generated_img_data/data/desc.json'
 
-    with open(desc_json_path, encoding="utf-8") as json_file:
+    with open(desc_json_path, encoding="utf-8", mode="r") as json_file:
         data = json.load(json_file)
 
     random.shuffle(data["train"])
     random.shuffle(data["test"])
 
-    with open(desc_json_path, 'w') as outfile:
+    with open(desc_json_path, encoding="uft-8", mode='w') as outfile:
         json.dump(data, outfile)
 
 
